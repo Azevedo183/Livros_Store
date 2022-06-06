87634696a26459dd5a29bccd9c8b275d6686bbb7
Noel Lopes <noel@ipg.pt>
2022-06-06 11:01:28 +0100

    Criada função para converter um Cursor numa Categoria

--- app/src/main/java/pt/ipg/livros/Categoria.kt
+++ app/src/main/java/pt/ipg/livros/Categoria.kt
@@ -1,6 +1,7 @@
 package pt.ipg.livros
 
 import android.content.ContentValues
+import android.database.Cursor
 import android.provider.BaseColumns
 
 data class Categoria(var nome: String, var id: Long = -1) {
@@ -9,5 +10,17 @@ data class Categoria(var nome: String, var id: Long = -1) {
         valores.put(TabelaBDCategorias.CAMPO_NOME, nome)
 
         return valores
+    }
+
+    companion object {
+        fun fromCursor(cursor: Cursor): Categoria {
+            val posId = cursor.getColumnIndex(BaseColumns._ID)
+            val posNome = cursor.getColumnIndex(TabelaBDCategorias.CAMPO_NOME)
+
+            val id = cursor.getLong(posId)
+            val nome = cursor.getString(posNome)
+
+            return Categoria(nome, id)
+        }
     }
 }
