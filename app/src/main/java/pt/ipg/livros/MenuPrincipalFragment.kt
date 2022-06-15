ad6c328dd75283724da532ea7aa385e36a00b440
Noel Lopes <noel@ipg.pt>
2022-06-15 12:27:59 +0100

    Ao clicar no botão livros mostrar (o fragmento com) a lista de livros

--- app/src/main/java/pt/ipg/livros/MenuPrincipalFragment.kt
+++ app/src/main/java/pt/ipg/livros/MenuPrincipalFragment.kt
@@ -32,9 +32,9 @@ class MenuPrincipalFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-//        binding.buttonFirst.setOnClickListener {
-//            findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
-//        }
+        binding.buttonLivros.setOnClickListener {
+            findNavController().navigate(R.id.action_MenuPrincipal_to_ListaLivros)
+        }
     }
 
     override fun onDestroyView() {
