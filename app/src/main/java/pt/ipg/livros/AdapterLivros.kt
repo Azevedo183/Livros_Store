5f98960f6705b926916c036dfaea1a590ccfd094
Noel Lopes <noel@ipg.pt>
2022-06-20 10:14:22 +0100

    Implementada função getItemCount do AdapterLivros

--- app/src/main/java/pt/ipg/livros/AdapterLivros.kt
+++ app/src/main/java/pt/ipg/livros/AdapterLivros.kt
@@ -77,6 +77,8 @@ class AdapterLivros : RecyclerView.Adapter<AdapterLivros.ViewHolderLivro>() {
      * @return The total number of items in this adapter.
      */
     override fun getItemCount(): Int {
-        TODO("Not yet implemented")
+        if (cursor == null) return 0
+
+        return cursor!!.count
     }
 }
